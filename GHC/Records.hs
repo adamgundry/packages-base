6b76685af8b9fc887db416ad32783a8fc6cf357d
Adam Gundry <adam.gundry@strath.ac.uk>
2013-08-01 13:48:49 +0100

    Support lenses and simple update

--- GHC/Records.hs
+++ GHC/Records.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE MultiParamTypeClasses, KindSignatures, DataKinds, TypeFamilies #-}
+{-# LANGUAGE MultiParamTypeClasses, KindSignatures, DataKinds,
+             TypeFamilies, ExplicitForAll #-}
 
-module GHC.Records (Has(..)) where
+module GHC.Records where
 
 import GHC.TypeLits (Symbol)
 
@@ -8,3 +9,13 @@ type family GetResult (r :: *) (f :: Symbol) :: *
 
 class t ~ GetResult r f => Has r (f :: Symbol) t where
   getField :: proxy f -> r -> t
+  setField :: proxy f -> r -> t -> r
+
+class Accessor (p :: * -> * -> *) where
+  accessor :: (r -> t) -> (r -> t -> r) -> p r t
+
+instance Accessor (->) where
+  accessor = const
+
+field :: forall proxy f r t p . (Has r f t, Accessor p) => proxy f -> p r t
+field z = accessor (getField z) (setField z)
