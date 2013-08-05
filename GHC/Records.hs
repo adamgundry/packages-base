f7ed3306b3b603425f1ab37435415a21ef421697
Adam Gundry <adam.gundry@strath.ac.uk>
2013-08-05 17:48:23 +0100

    Type-changing update

--- GHC/Records.hs
+++ GHC/Records.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE MultiParamTypeClasses, KindSignatures, DataKinds,
-             TypeFamilies, ExplicitForAll #-}
+             TypeFamilies, RankNTypes, FlexibleInstances, FlexibleContexts #-}
 
 module GHC.Records where
 
@@ -7,15 +7,24 @@ import GHC.TypeLits (Symbol)
 
 type family GetResult (r :: *) (f :: Symbol) :: *
 
-class t ~ GetResult r f => Has r (f :: Symbol) t where
+class (t ~ GetResult r f, r ~ SetResult r f t) =>
+          Has r (f :: Symbol) t where
   getField :: proxy f -> r -> t
-  setField :: proxy f -> r -> t -> r
 
-class Accessor (p :: * -> * -> *) where
-  accessor :: (r -> t) -> (r -> t -> r) -> p r t
+type family SetResult (r :: *) (f :: Symbol) (t :: *) :: *
 
-instance Accessor (->) where
-  accessor = const
+class Has r f (GetResult r f) => Upd (r :: *) (f :: Symbol) (t :: *) where
+  setField :: proxy f -> r -> t -> SetResult r f t
 
-field :: forall proxy f r t p . (Has r f t, Accessor p) => proxy f -> p r t
-field z = accessor (getField z) (setField z)
+
+class Accessor (p :: * -> * -> *) (f :: Symbol) where
+  accessor :: proxy f -> (r -> GetResult r f) ->
+              (forall t . Upd r f t => r -> t -> SetResult r f t) ->
+              p r (GetResult r f)
+
+instance Accessor (->) f where
+  accessor _ getter _ = getter
+
+
+field :: forall proxy f r t p . (Has r f t, Accessor p f) => proxy f -> p r t
+field z = accessor z (getField z) (setField z)
